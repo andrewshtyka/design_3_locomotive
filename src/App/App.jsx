b0b10b58187de0d34c47831fe7fcbfbe4a167539
Andrew Shtyka <andrii.shtyka@gmail.com>
2025-10-14 11:58:33 +0300

    adaptivity 2400

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,11 +3,11 @@ import Hero from "../Components/Hero/Hero";
 import MainSection from "../Components/MainSection/MainSection";
 
 export default function App() {
-  return (
-    <div>
-      <Header />
-      <Hero />
-      <MainSection />
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Hero />
+			<MainSection />
+		</div>
+	);
 }
