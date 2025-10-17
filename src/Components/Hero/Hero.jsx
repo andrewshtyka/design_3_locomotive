31af5bc4f7f53bed4489f65458f80a1e05fd2443
Andrew Shtyka <andrii.shtyka@gmail.com>
2025-10-17 17:29:43 +0300

    fix video safari 6

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import css from "./Hero.module.css";
 import posterDesktop from "../../Assets/Video/poster_locomotive_desktop.png";
 import posterMobile from "../../Assets/Video/poster_locomotive_mobile.png";
@@ -8,6 +8,17 @@ export const Hero = forwardRef(({ className, isMobile }, ref) => {
 		"https://player.vimeo.com/progressive_redirect/playback/792718372/rendition/1080p/file.mp4?loc=external&log_user=0&signature=978abf9e4b33e3e143901fbcbf68e159d90d5eeb95ed25f8378d341514009cf8";
 
 	const poster = isMobile ? posterMobile : posterDesktop;
+	const [showVideo, setShowVideo] = useState(true);
+
+	useEffect(() => {
+		if (navigator.getBattery) {
+			navigator.getBattery().then((battery) => {
+				if (battery.level < 0.2 || battery.savePowerMode) {
+					setShowVideo(false);
+				}
+			});
+		}
+	}, []);
 
 	return (
 		<section ref={ref} className={`${css.c_container} ${className ? " " + className : ""}`}>
@@ -19,7 +30,11 @@ export const Hero = forwardRef(({ className, isMobile }, ref) => {
 			</h1>
 
 			<div className={css.o_video_container}>
-				<video className={css.o_video} src={videoSrc} autoPlay playsInline muted loop poster={poster} />
+				{showVideo ? (
+					<video className={css.o_video} src={videoSrc} autoPlay playsInline muted loop poster={poster} />
+				) : (
+					<img className={css.o_video} src={poster} alt="Poster" />
+				)}
 			</div>
 		</section>
 	);
